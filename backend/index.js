6985851c675736a570d01e3cabc5ecb27e645579
Jacky Xie <gokurohono@gmail.com>
2021-08-21 17:54:05 +1000

    a

--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,38 @@
-
-const express = require('express')
+const cors = require('cors')
+const express = require('express');
 const app = express();
+
+/*
+// enable cors
+app.use(cors())
 const port = 8080;
 
 // server
 const http = require('http');
 const server = http.createServer(app);
-const socketio = require('socket.io');
-const io = socketio(server);
 
-
-app.get('/', (req, res)=> {
-    res.send('hello world').status(200);
-})
-
-/*
-class Connection {
-    constructor(io, socket) {
-        this.socket = socket;
-        this.io = io;
-        
-        console.log('user has connected');
-        socket.on('disconnect', ()=> this.disconect());
-        socket.on('dostuff', () => this.dostuff());
+const io = require('socket.io')(server, {
+    cors: {
+        origin: ['http://localhost:3000']
     }
-    connection() {
-        console.log('user has connected')
-    }
-
-    disconnect() {
-        console.log('user has disconnected')
-    }
-
-    dostuff() {
-        console.log('hi')
-    }
-
-}
-*/
-
-io.on('connection', (socket) => {
-    // new Connection(io, socket)
-    console.log('user has connected')
-    socket.on('disconnect', ()=> {
-        console.log('user has disconnected');
-    })
 });
 
 
 
+
+io.on('connection', (socket) => {
+    console.log('new connection established');
+});
+
 server.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
+})
+
+*/
+
+const io = require('socket.io')(5000);
+
+io.on('connect', (socket)=> {
+    console.log('new connection')
+    io.emit('a', null);
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import { socket } from './socket'
+
 
 function App() {
+
+  useEffect(()=> {
+    // 
+    console.log(socket.connected)
+    socket.on('a', ()=> {
+      console.log('hi');
+    })
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        hi
+      </div>
     </div>
   );
 }
 
 export default App;
+
